1499d3718829081bd09e8b97e0e5c97a9ef12b84
rafelafrance <raphael.lafrance@gmail.com>
2024-06-30 21:39:03 -0400

    Reduce slurm requirement further

--- args/slurm_vit_224_train_fruits.bash
+++ args/slurm_vit_224_train_fruits.bash
@@ -12,8 +12,8 @@
 #SBATCH --output=/blue/guralnick/rafe.lafrance/phenobase/logs/%x_%j.out
 
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=16gb
-#SBATCH --time=24:00:00
+#SBATCH --mem=4gb
+#SBATCH --time=12:00:00
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:a100:1
 
