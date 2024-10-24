0f3cefa14d7cb6138df58db5d21704d140ab5796
rafelafrance <raphael.lafrance@gmail.com>
2024-10-24 16:46:52 -0400

    Add a utility for updating gbif_db's multimedia dir field

---
+++ args/gbif/update_gbif_dir.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#SBATCH --account=guralnick
+#SBATCH --qos=guralnick
+
+#SBATCH --job-name=download_gbif_sheets
+
+#SBATCH --mail-user=rafe.lafrance@ufl.edu
+#SBATCH --mail-type=FAIL,END
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --output=/blue/guralnick/rafe.lafrance/phenobase/logs/%x_%j.out
+
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16gb
+#SBATCH --time=16:00:00
+
+date;hostname;pwd
+
+export PATH=/blue/guralnick/rafe.lafrance/.conda/envs/vitmae/bin:$PATH
+
+module purge
+
+python3 /blue/guralnick/rafe.lafrance/phenobase/phenobase/update_gbif_dir.py \
+  --gbif-db /blue/guralnick/rafe.lafrance/phenobase/data/gbif_2024-10-23.sqlite \
+  --image-dir /blue/guralnick/share/phenobase_specimen_data/images
+
+date
