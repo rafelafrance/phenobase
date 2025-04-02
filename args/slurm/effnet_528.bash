bb6b6b243e4d5ad02cfb36e6ea714ccb32e4982d
rafelafrance <raphael.lafrance@gmail.com>
2025-04-01 21:15:04 -0400

    Update args to use single label scoring

--- args/slurm/effnet_528.bash
+++ args/slurm/effnet_528.bash
@@ -3,7 +3,7 @@
 #SBATCH --account=guralnick
 #SBATCH --qos=guralnick
 
-#SBATCH --job-name=effnet_528_flowers_f1
+#SBATCH --job-name=effnet_528_flowers_sl_f1
 
 #SBATCH --mail-user=rafe.lafrance@ufl.edu
 #SBATCH --mail-type=ALL
@@ -24,7 +24,7 @@ export PATH=/blue/guralnick/rafe.lafrance/.conda/envs/vitmae/bin:$PATH
 module purge
 
 python3 /blue/guralnick/rafe.lafrance/phenobase/phenobase/model_train.py \
-  --output-dir /blue/guralnick/rafe.lafrance/phenobase/data/models/effnet_528_flowers_f1 \
+  --output-dir /blue/guralnick/rafe.lafrance/phenobase/data/models/effnet_528_flowers_sl_f1 \
   --image-dir /blue/guralnick/rafe.lafrance/phenobase/data/images/phenobase \
   --dataset-csv /blue/guralnick/rafe.lafrance/phenobase/datasets/train_data.csv \
   --finetune "google/efficientnet-b6" \
