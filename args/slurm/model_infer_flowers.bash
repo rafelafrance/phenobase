94cac26d1889890ec506ff7f7b7fabf017909af2
rafelafrance <raphael.lafrance@gmail.com>
2025-04-10 19:19:02 -0400

    Update inference arguments

--- args/slurm/model_infer_flowers.bash
+++ args/slurm/model_infer_flowers.bash
@@ -6,7 +6,7 @@
 #SBATCH --job-name=infer_flowers_0_10000
 
 #SBATCH --mail-user=rafe.lafrance@ufl.edu
-#SBATCH --mail-type=FAIL,END
+#SBATCH --mail-type=ALL
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --output=/blue/guralnick/rafe.lafrance/phenobase/logs/%x_%j.out
@@ -29,7 +29,7 @@ python3 /blue/guralnick/rafe.lafrance/phenobase/phenobase/model_infer.py \
     --db /home/rafe.lafrance/blue/phenobase/data/gbif_2024-10-28.sqlite \
     --image-dir /blue/guralnick/share/phenobase_specimen_data/images \
     --bad-families /home/rafe.lafrance/blue/phenobase/datasets/bad_families/bad_flower_fams.csv \
-    --output-csv /home/rafe.lafrance/blue/phenobase/data/flower_inference_0_10000.csv \
+    --output-csv /home/rafe.lafrance/blue/phenobase/data/infer/flower_inference_0_10000.csv \
     --checkpoint /blue/guralnick/rafe.lafrance/phenobase/data/models/effnet_528_flowers_f1_sl/checkpoint-15260 \
     --image-size 528 \
     --limit 1000000 \
