941815d7c9f8e60863e2e9ba01e5b78664f93a9f
rafelafrance <raphael.lafrance@gmail.com>
2025-04-12 06:55:27 -0400

    Change inference file name

--- args/slurm/model_infer_flowers.bash
+++ args/slurm/model_infer_flowers.bash
@@ -29,7 +29,7 @@ python3 /blue/guralnick/rafe.lafrance/phenobase/phenobase/model_infer.py \
     --db /home/rafe.lafrance/blue/phenobase/data/gbif_2024-10-28.sqlite \
     --image-dir /blue/guralnick/share/phenobase_specimen_data/images \
     --bad-families /home/rafe.lafrance/blue/phenobase/datasets/bad_families/bad_flower_fams.csv \
-    --output-csv /home/rafe.lafrance/blue/phenobase/data/infer/flower_inference_0_10000.csv \
+    --output-csv /home/rafe.lafrance/blue/phenobase/data/infer/flower_inference_0_10000_2025-04-12.csv \
     --checkpoint /blue/guralnick/rafe.lafrance/phenobase/data/models/effnet_528_flowers_f1_sl/checkpoint-15260 \
     --image-size 528 \
     --limit 100000 \
