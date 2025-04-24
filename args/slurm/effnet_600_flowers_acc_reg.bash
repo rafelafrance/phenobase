e2a1751c2f4dc76878f088a762473f9fdde51dac
rafelafrance <raphael.lafrance@gmail.com>
2025-04-24 11:09:05 -0400

    Add more scripts for different model parameters

---
+++ args/slurm/effnet_600_flowers_acc_reg.bash
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#SBATCH --account=guralnick
+#SBATCH --qos=guralnick
+
+#SBATCH --job-name=effnet_600_flowers_acc_reg
+
+#SBATCH --mail-user=rafe.lafrance@ufl.edu
+#SBATCH --mail-type=ALL
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --output=/blue/guralnick/rafe.lafrance/phenobase/logs/%x_%j.out
+
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=4gb
+#SBATCH --time=36:00:00
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:a100:1
+
+date;hostname;pwd
+
+export PATH=/blue/guralnick/rafe.lafrance/.conda/envs/vitmae/bin:$PATH
+
+module purge
+
+python3 /blue/guralnick/rafe.lafrance/phenobase/phenobase/model_train.py \
+  --output-dir /blue/guralnick/rafe.lafrance/phenobase/data/models/effnet_600_flowers_acc_reg \
+  --image-dir /blue/guralnick/rafe.lafrance/phenobase/data/images/phenobase \
+  --dataset-csv /blue/guralnick/rafe.lafrance/phenobase/datasets/splits_2025-04-22.csv \
+  --finetune "google/efficientnet-b7" \
+  --image-size 600 \
+  --epochs 100 \
+  --trait flowers \
+  --best-metric accuracy \
+  --problem-type regression \
+  --batch-size 16
+
+date
