b8d7c68674d6d53bf8a924c0c5fef73f8922ea28
rafelafrance <raphael.lafrance@gmail.com>
2025-04-25 12:12:05 -0400

    Add more/different models for an ensemble

---
+++ args/slurm/vit_224_lg_flowers_acc_reg.bash
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#SBATCH --account=guralnick
+#SBATCH --qos=guralnick
+
+#SBATCH --job-name=vit_224_lg_flowers_acc_reg
+
+#SBATCH --mail-user=rafe.lafrance@ufl.edu
+#SBATCH --mail-type=ALL
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --output=/blue/guralnick/rafe.lafrance/phenobase/logs/%x_%j.out
+
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=4gb
+#SBATCH --time=36:00:00
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:a100:1
+
+date
+hostname
+pwd
+
+export PATH=/blue/guralnick/rafe.lafrance/.conda/envs/vitmae/bin:$PATH
+
+module purge
+
+python3 /blue/guralnick/rafe.lafrance/phenobase/phenobase/model_train.py \
+    --output-dir /blue/guralnick/rafe.lafrance/phenobase/data/models/vit_224_lg_flowers_acc_reg \
+    --image-dir /blue/guralnick/rafe.lafrance/phenobase/data/images/phenobase \
+    --dataset-csv /blue/guralnick/rafe.lafrance/phenobase/datasets/splits_2025-04-22.csv \
+    --finetune "google/vit-large-patch16-224" \
+    --image-size 224 \
+    --batch-size 32 \
+    --best-metric accuracy \
+    --problem-type regression \
+    --epochs 100 \
+    --trait flowers
+
+date
