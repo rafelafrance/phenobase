9d5f3e31a0812f6df2f6726d031bf045357d5fd2
rafelafrance <raphael.lafrance@gmail.com>
2025-05-09 09:38:04 -0400

    Add scripts for new ensemble

---
+++ args/slurm/flowers/infer_ensemble_fract_1.bash
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#SBATCH --account=guralnick
+#SBATCH --qos=guralnick
+
+#SBATCH --job-name=flower_inference_fract_10000_30000_1
+
+#SBATCH --mail-user=rafe.lafrance@ufl.edu
+#SBATCH --mail-type=ALL
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --output=/blue/guralnick/rafe.lafrance/phenobase/logs/%x_%j.out
+
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=64gb
+#SBATCH --time=24:10:00
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:a100:1
+
+date
+hostname
+pwd
+
+export PATH=/blue/guralnick/rafe.lafrance/.conda/envs/vitmae/bin:$PATH
+
+module purge
+
+python3 /blue/guralnick/rafe.lafrance/phenobase/phenobase/model_infer.py \
+    --db /home/rafe.lafrance/blue/phenobase/data/gbif_2024-10-28.sqlite \
+    --image-dir /blue/guralnick/share/phenobase_specimen_data/images \
+    --bad-taxa /home/rafe.lafrance/blue/phenobase/datasets/remove_flowers.csv \
+    --output-csv /home/rafe.lafrance/blue/phenobase/data/infer/flower_inference_fract_10000_30000_1_2025-05-09.csv \
+    --checkpoint /blue/guralnick/rafe.lafrance/phenobase/data/models/best_3combo_fract/effnet_528_flowers_reg_f1_a_checkpoint-17424 \
+    --image-size 528 \
+    --limit 10000 \
+    --offset 30000 \
+    --problem-type regression \
+    --trait flowers
+
+date
---
+++ args/slurm/flowers/infer_ensemble_fract_3.bash
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#SBATCH --account=guralnick
+#SBATCH --qos=guralnick
+
+#SBATCH --job-name=flower_inference_fract_10000_30000_3
+
+#SBATCH --mail-user=rafe.lafrance@ufl.edu
+#SBATCH --mail-type=ALL
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --output=/blue/guralnick/rafe.lafrance/phenobase/logs/%x_%j.out
+
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=64gb
+#SBATCH --time=24:10:00
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:a100:1
+
+date
+hostname
+pwd
+
+export PATH=/blue/guralnick/rafe.lafrance/.conda/envs/vitmae/bin:$PATH
+
+module purge
+
+python3 /blue/guralnick/rafe.lafrance/phenobase/phenobase/model_infer.py \
+    --db /home/rafe.lafrance/blue/phenobase/data/gbif_2024-10-28.sqlite \
+    --image-dir /blue/guralnick/share/phenobase_specimen_data/images \
+    --bad-taxa /home/rafe.lafrance/blue/phenobase/datasets/remove_flowers.csv \
+    --output-csv /home/rafe.lafrance/blue/phenobase/data/infer/flower_inference_fract_10000_30000_3_2025-05-09.csv \
+    --checkpoint /blue/guralnick/rafe.lafrance/phenobase/data/models/best_3combo_fract/vit_384_lg_flowers_f1_slurm_sl_checkpoint-9450 \
+    --image-size 384 \
+    --limit 10000 \
+    --offset 30000 \
+    --trait flowers
+
+date
