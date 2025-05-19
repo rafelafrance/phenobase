bfe6622072eeadfd908d8ff44c1e9f54ada456b6
rafelafrance <raphael.lafrance@gmail.com>
2025-05-19 12:58:49 -0400

    Script for more images to audit

--- args/ensemble_inferred_audit_best_3combo_fract.bash
+++ args/ensemble_inferred_audit_best_3combo_fract.bash
@@ -2,7 +2,7 @@
 
 ./phenobase/ensemble_inferred_audit.py \
   --ensemble-json data/scores/best_3combo_fract.json \
-  --score-csv data/inference/best_3combo_fract/flower_inference_fract_10000_30000_3_2025-05-09.csv \
-  --score-csv data/inference/best_3combo_fract/flower_inference_fract_10000_30000_2_2025-05-09.csv \
-  --score-csv data/inference/best_3combo_fract/flower_inference_fract_10000_30000_1_2025-05-09.csv \
-  --output-csv data/inference/best_3combo_fract/flower_inference_fract_10000_30000_votes2.csv
+  --score-csv data/inference/best_3combo_fract/flower_inference_fract_15000_40000_3_2025-05-19.csv \
+  --score-csv data/inference/best_3combo_fract/flower_inference_fract_15000_40000_2_2025-05-19.csv \
+  --score-csv data/inference/best_3combo_fract/flower_inference_fract_15000_40000_1_2025-05-19.csv \
+  --output-csv data/inference/best_3combo_fract/flower_inference_fract_15000_40000_votes3.csv
--- args/ensemble_inferred_sample.bash
+++ args/ensemble_inferred_sample.bash
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 ./phenobase/ensemble_inferred_sample.py \
-  --winners-csv data/inference/best_3combo_fract/flower_inference_fract_10000_30000_votes2.csv \
-  --exclude-csv data/inference/best_3combo_fract/flower_ensemble_fract_sample_pos.csv \
-  --image-dir /blue/guralnick/rafe.lafrance/phenobase/data/images/flower_inference_fract \
+  --winners-csv data/inference/best_3combo_fract/flower_inference_fract_15000_40000_votes3.csv \
+  --image-dir /blue/guralnick/rafe.lafrance/phenobase/data/images/flower_inference_fract3 \
   --image-bash args/slurm/move_flower_ensemble_fract_images.bash \
-  --output-sample data/inference/best_3combo_fract/flower_ensemble_fract_sample2.csv \
-  --sample-positive 100
+  --output-sample data/inference/best_3combo_fract/flower_ensemble_fract_sample3.csv \
+  --sample-positive 400
