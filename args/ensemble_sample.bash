a4f5f6967105bbb4c005f63035286dcd66308665
rafelafrance <raphael.lafrance@gmail.com>
2025-06-05 15:01:03 -0400

    Sample from the inference dataset

--- args/ensemble_sample.bash
+++ args/ensemble_sample.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ./phenobase/ensemble_sample.py \
-  --winners-csv data/custom/flower_redbud_ensemble_votes.csv \
-  --image-dir /blue/guralnick/rafe.lafrance/phenobase/data/images/flower_neg_sample \
+  --winners-csv data/inference/best_3combo_fract/flower_inference_fract_15000_40000_votes3.csv \
+  --image-dir /blue/guralnick/rafe.lafrance/phenobase/data/images/flower_neg_sample2 \
   --image-bash args/slurm/move_flower_ensemble_neg_sample.bash \
-  --output-sample data/custom/flower_neg_sample.csv \
+  --output-sample data/inference/best_3combo_fract/flower_neg_sample2.csv \
   --sample-negative 500
