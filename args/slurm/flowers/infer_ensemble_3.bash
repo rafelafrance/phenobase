ac9d44416db765ab32269a020d16a62e5f9f7fc9
rafelafrance <raphael.lafrance@gmail.com>
2025-06-11 10:19:25 -0400

    Update output file names

--- args/slurm/flowers/infer_ensemble_3.bash
+++ args/slurm/flowers/infer_ensemble_3.bash
@@ -26,7 +26,7 @@ python3 /blue/guralnick/rafe.lafrance/phenobase/phenobase/model_infer.py \
     --db /home/rafe.lafrance/blue/phenobase/data/gbif_2024-10-28.sqlite \
     --image-dir /blue/guralnick/share/phenobase_specimen_data/images \
     --bad-taxa /home/rafe.lafrance/blue/phenobase/datasets/remove_flowers.csv \
-    --output-csv /home/rafe.lafrance/blue/phenobase/data/infer/flower_inference_1000000_0000000_3_2025-06-10.csv \
+    --output-csv /home/rafe.lafrance/blue/phenobase/data/infer/flower_inference_1000000_0000000_3_2025-06-11.csv \
     --checkpoint /blue/guralnick/rafe.lafrance/phenobase/data/models/best_3combo_fract/vit_384_lg_flowers_f1_slurm_sl_checkpoint-9450 \
     --image-size 384 \
     --limit 1000000 \
