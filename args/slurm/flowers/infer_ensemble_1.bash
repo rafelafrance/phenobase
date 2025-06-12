a0e7a8c32c35667e3ced37feb218665130063d04
rafelafrance <raphael.lafrance@gmail.com>
2025-06-12 10:31:15 -0400

    Double up on the inference runs

--- args/slurm/flowers/infer_ensemble_1.bash
+++ args/slurm/flowers/infer_ensemble_1.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=flower_inference_1000000_0000000_1
+#SBATCH --job-name=flower_inference_1000000_1000000_1
 
 #SBATCH --mail-user=rafe.lafrance@ufl.edu
 #SBATCH --mail-type=ALL
@@ -10,7 +10,7 @@
 
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=64gb
-#SBATCH --time=3-00:00:00
+#SBATCH --time=4-00:00:00
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:a100:1
 
@@ -26,11 +26,25 @@ python3 /blue/guralnick/rafe.lafrance/phenobase/phenobase/model_infer.py \
     --db /home/rafe.lafrance/blue/phenobase/data/gbif_2024-10-28.sqlite \
     --image-dir /blue/guralnick/share/phenobase_specimen_data/images \
     --bad-taxa /home/rafe.lafrance/blue/phenobase/datasets/remove_flowers.csv \
-    --output-csv /home/rafe.lafrance/blue/phenobase/data/infer/flower_inference_1000000_0000000_1_2025-06-11.csv \
+    --output-csv /home/rafe.lafrance/blue/phenobase/data/infer/flower_inference_1000000_1000000_1_2025-06-11.csv \
     --checkpoint /blue/guralnick/rafe.lafrance/phenobase/data/models/best_3combo_fract/effnet_528_flowers_reg_f1_a_checkpoint-17424 \
     --image-size 528 \
     --limit 1000000 \
-    --offset 0 \
+    --offset 1000000 \
+    --problem-type regression \
+    --trait flowers
+
+date
+
+python3 /blue/guralnick/rafe.lafrance/phenobase/phenobase/model_infer.py \
+    --db /home/rafe.lafrance/blue/phenobase/data/gbif_2024-10-28.sqlite \
+    --image-dir /blue/guralnick/share/phenobase_specimen_data/images \
+    --bad-taxa /home/rafe.lafrance/blue/phenobase/datasets/remove_flowers.csv \
+    --output-csv /home/rafe.lafrance/blue/phenobase/data/infer/flower_inference_1000000_2000000_1_2025-06-11.csv \
+    --checkpoint /blue/guralnick/rafe.lafrance/phenobase/data/models/best_3combo_fract/effnet_528_flowers_reg_f1_a_checkpoint-17424 \
+    --image-size 528 \
+    --limit 1000000 \
+    --offset 2000000 \
     --problem-type regression \
     --trait flowers
 
