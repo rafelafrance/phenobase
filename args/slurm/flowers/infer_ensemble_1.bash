c70c0e0050460b62bb00b613b73846fbee20cc60
rafelafrance <raphael.lafrance@gmail.com>
2025-06-30 12:46:11 -0400

    Next round of inference

--- args/slurm/flowers/infer_ensemble_1.bash
+++ args/slurm/flowers/infer_ensemble_1.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=flower_inference_1a
+#SBATCH --job-name=flower_inference_1b
 
 #SBATCH --mail-user=rafe.lafrance@ufl.edu
 #SBATCH --mail-type=ALL
@@ -10,7 +10,7 @@
 
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=16gb
-#SBATCH --time=2-00:00:00
+#SBATCH --time=10-00:00:00
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 
@@ -26,11 +26,11 @@ python3 /blue/guralnick/rafe.lafrance/phenobase/phenobase/model_infer.py \
     --gbif-db /home/rafe.lafrance/blue/phenobase/data/gbif_2024-10-28.sqlite \
     --image-dir /blue/guralnick/share/phenobase_specimen_data/images \
     --bad-taxa /home/rafe.lafrance/blue/phenobase/datasets/remove_flowers.csv \
-    --output-csv /home/rafe.lafrance/blue/phenobase/data/infer/flower_inference_1a.csv \
+    --output-csv /home/rafe.lafrance/blue/phenobase/data/infer/flower_inference_1b.csv \
     --checkpoint /blue/guralnick/rafe.lafrance/phenobase/data/models/best_3combo_fract/effnet_528_flowers_reg_f1_a_checkpoint-17424 \
     --image-size 528 \
-    --limit 1000000 \
-    --offset 0 \
+    --limit 4000000 \
+    --offset 1000000 \
     --problem-type regression \
     --trait flowers
 
